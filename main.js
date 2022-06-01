458def5d534c01d67f9d18117280c7be27e7a073
Hassan <hassan.seedat07@gmail.com>
2022-06-01 11:06:03 +0100

    completed first 3 challenges

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 const arrays = ["apple", "banana", "pear", "kiwi"]
 const myArray = arrays.map((array) => {
     for (let i = 0; i < arrays.length; i++) {
-        arrays[i] = arrays[i][0].toUpperCase()
+        arrays[i] = arrays[i].toUpperCase()
     }return arrays;
 });
 
@@ -16,7 +16,7 @@ console.log(myArray);
 // Takes an array of letters as a parameter.
 // It needs return a new array with only consonants, the vowels need to be removed.
 
-const arraysFruit = ["apple", "banana", "pear", "kiwi", "sdfgh"]
+const arraysFruit = ["apple", "banana", "pear", "kiwi"]
 const removeVowels = arraysFruit.filter((Fruit) => {
     return Fruit.includes("a", "e", "i", "o", "u");
 })
@@ -28,3 +28,12 @@ console.log(removeVowels)
 // Takes an array of numbers as a parameter
 // It needs to return the mean average
 // Total the numbers and divide by the amount of numbers in the array
+
+const numbers = [2,35,3,5,9]
+
+const mean = (arr) => {
+    return arr.reduce((total, current) => total += current, 0) / arr.length;
+
+}
+
+console.log(mean(numbers));
